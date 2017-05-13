c629f0c9a9418d7edc1924525685b93f997fafe5
rsstdd <rosstod@gmail.com>
2017-05-13 13:51:17 -0700

    Add game logic; Troubleshoot MQTT

--- src/app.js
+++ src/app.js
@@ -1,25 +1,15 @@
-const senseJoystick = require('sense-joystick');
-const senseLeds = require('sense-hat-led');
-const Promise = require('bluebird');
-const senseJoystick = require('sense-joystick');
-const weapons = [rock, paper, scissors];
-const rock { img: [], } const paper { img: [], } const scissors { img: [] }
-const outcomes = [
-  rock: { scissors: true, paper: false, },
-  scissors: { paper: true, rock: false, },
-  paper: { rock: true, scissors: false, }
-];
-const winColor = [0, 102, 0];
-const loseColor = [255, 255, 255];
-const tieColor = [255, 255, 0];
+const mqtt = require('mqtt')
+const client  = mqtt.connect('mqtt://test.mosquitto.org')
 
-// ---------------------------------------------------
+client.on('connect', () => {
+  client.subscribe('presence')
 
-const selectWeapon = (weapons) => {
-  senseJoystick.getJoystick()
-  .then((joystick) => {
-    joystick.on('press', (direction) => {
-      console.log('Got button press in the direction: ', direction);
-    }
-  });
-}
+  client.publish('presence', 'Hello mqtt')
+})
+
+client.on('message', (topic, message) => {
+	// buff toString
+
+	console.log(message.toString())
+  client.end()
+})
