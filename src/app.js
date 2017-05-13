3e4c5c6276b634e2adf7ae8aed59277cd682c4f7
rsstdd <rosstod@gmail.com>
2017-05-13 14:53:04 -0700

    network

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 const mqtt = require('mqtt')
 const os = require('os').networkInterfaces()
-const client  = mqtt.connect({hostname: os.wlan0[0].address })
+const client  = mqtt.connect({ hostname: os.wlan0[0].address })
 
 client.on('connect', () => {
   client.subscribe('presence')
